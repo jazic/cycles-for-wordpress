5e3ce21afa35f681e19f2294156927cace046db9
Justin <justinziccardi@gmail.com>
2016-11-04 14:08:55 -0600

    repo assets

--- cycles.php
+++ cycles.php
@@ -8,17 +8,17 @@
  * registers the activation and deactivation functions, and defines a function
  * that starts the plugin.
  *
- * @link              http://example.com
- * @since             3.0
+ * @link              https://cycleshq.com
+ * @since             1.0
  * @package           Cycles
  *
  * @wordpress-plugin
- * Plugin Name:       Cycles 
- * Plugin URI:        https://demo.cycleshq.com/
+ * Plugin Name:       Cycles
+ * Plugin URI:        https://cycleshq.com/
  * Description:       The WordPress plugin will help users install  the script so they don’t have to mess with any code
- * Version:           3.0
- * Author:            Pawan Vir
- * Author URI:        https://demo.cycleshq.com/ 
+ * Version:           1.0
+ * Author:            Cycles
+ * Author URI:        https://cycleshq.com/
  * Text Domain:       cycles
  * Domain Path:       /languages
  */
@@ -27,7 +27,7 @@
 if ( ! defined( 'WPINC' ) ) {
 	die;
 }
-$plugin = plugin_basename(__FILE__); 
+$plugin = plugin_basename(__FILE__);
 /**
  * The code that runs during plugin activation.
  * This action is documented in includes/class-cycles-activator.php
@@ -50,11 +50,11 @@ function deactivate_cycles() {
  * The code that runs during plugin settings.
  * This action is documented in includes/class-cycles-deactivator.php
  */
- 
-function cycles_settings_link($links) { 
-  $settings_link = '<a href="options-general.php?page=cycles">Settings</a>'; 
-  array_push($links, $settings_link); 
-  return $links; 
+
+function cycles_settings_link($links) {
+  $settings_link = '<a href="options-general.php?page=cycles">Settings</a>';
+  array_push($links, $settings_link);
+  return $links;
 }
 
 add_filter("plugin_action_links_$plugin", 'cycles_settings_link');
@@ -66,7 +66,7 @@ register_deactivation_hook( __FILE__, 'deactivate_cycles' );
  * The core plugin class that is used to define internationalization,
  * admin-specific hooks, and public-facing site hooks.
  */
-require plugin_dir_path( __FILE__ ) . 'includes/class-cycles.php'; 
+require plugin_dir_path( __FILE__ ) . 'includes/class-cycles.php';
 
 /**
  * Begins execution of the plugin.
